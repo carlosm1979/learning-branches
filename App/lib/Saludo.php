193358a9a41e96a1c86191487323b40afd8926f6
Carlos Gonzalez <carlosm1979@gmail.com>
2015-12-31 14:43:14 +0100

    Clase Saludo

---
+++ App/lib/Saludo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\lib;
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 31/12/2015
+ * Time: 14:36
+ */
+class Saludo
+{
+    public function saludar(){
+        echo "hola";
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 31/12/2015
+ * Time: 14:36
+ */
+
+include("App\lib\Saludo.php");
+
+use App\lib\Saludo;
+
+
+$saludo = new Saludo();
+
+$saludo->saludar();
