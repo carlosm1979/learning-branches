f1c8e3a91e941a4c3cb7651da88b718efb40b2a9
Carlos Gonzalez <carlosm1979@gmail.com>
2016-01-01 14:00:41 +0100

    Clase Saludo

---
+++ App/lib/Despedida.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 01/01/2016
+ * Time: 13:59
+ */
+
+namespace App\lib;
+
+
+class Despedida
+{
+
+    public function chao(){
+        echo "Chao";
+    }
+
+}
